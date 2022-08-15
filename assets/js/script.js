a37b7272d7bb55f1bdd2abf18bdafcd8c9c367e9
SabaChoxeli <sabachoxeli13@gmail.com>
2022-08-15 19:23:49 +0400

    update header images and add slider

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const sliders = document.querySelectorAll(".slider-item");
+let activeIndex = 0,
+  autoSlidingId = null,
+  isImgsSliding = true;
+
+function renderSlides() {
+  sliders.forEach((el, i) => {
+    if (i === activeIndex) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+}
+
+function nextSlide() {
+  if (activeIndex === sliders.length - 1) {
+    activeIndex = 0;
+  } else {
+    activeIndex++;
+  }
+  renderSlides();
+}
+
+function startSliding() {
+  autoSlidingId = setInterval(nextSlide, 5000);
+  isImgsSliding = true;
+}
+
+startSliding();
