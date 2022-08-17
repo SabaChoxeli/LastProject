a369c775fe8b85506ee9da2d13ee6f47d1e006f1
SabaChoxeli <sabachoxeli13@gmail.com>
2022-08-17 17:22:53 +0400

    add js on recommendation section, add hover on latest projects,add hover on rosa team,

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,37 @@ startSliding();
 // progressBar.addEventListener("click", () => {
 //   progressBar.classList.add("active");
 // });
+
+const squareButton = document.querySelectorAll(".square-button"),
+  profiles = document.querySelectorAll(".recom-profiles");
+
+squareButton.forEach((el, i) => {
+  el.addEventListener("click", () => {
+    changeSquare(i);
+  });
+});
+
+function changeSquare(nextIndex) {
+  activeIndex = nextIndex;
+  renderButtons();
+}
+
+function renderButtons() {
+  profiles.forEach((el, i) => {
+    if (activeIndex === i) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+
+  squareButton.forEach((el, i) => {
+    if (activeIndex === i) {
+      el.classList.add("active");
+    } else {
+      el.classList.remove("active");
+    }
+  });
+}
+
+console.log("sqd");
